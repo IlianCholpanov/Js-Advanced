bb54b7dc5fcd2a240b8140d746e87fe4a77ebcd8
IlianCholpanov <iliancholpanov@gmail.com>
2023-09-02 13:27:22 +0300

    .

--- Exercise Advanced Functions/08. Next Article/script.js
+++ Exercise Advanced Functions/08. Next Article/script.js
@@ -1,3 +1,14 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let input = articles;
+
+    return () => {
+        if(input.length > 0){
+            let container = document.getElementById('content');
+            let article = document.createElement('article');
+            let currentText = articles.shift();
+            console.log(currentText);
+            article.innerText = currentText;
+            container.appendChild(article);
+        }
+    };
 }
