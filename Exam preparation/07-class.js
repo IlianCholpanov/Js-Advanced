2a98f92d623e2fa81ae2f4354f2d3a0bee5ed71e
Ilian Cholpanov <iliancholpanov@gmail.com>
2023-12-02 15:12:02 +0200

    Update 07-class.js

--- Exam preparation/07-class.js
+++ Exam preparation/07-class.js
@@ -1,1 +1,72 @@
-class Garden {}
+class Garden {
+  constructor(spaceAvailable) {
+    this.spaceAvailable = Number(spaceAvailable);
+    this.plants = [];
+    this.storage = [];
+  }
+
+  addPlant(plantName, spaceRequired) {
+    if (this.spaceAvailable < Number(spaceRequired)) {
+      throw new Error("Not enough space in the garden.");
+    }
+
+    this.plants.push({ plantName, spaceRequired, ripe: false, quantity: 0 });
+    this.spaceAvailable -= spaceRequired;
+
+    return `The ${plantName} has been successfully planted in the garden.`;
+  }
+
+  ripenPlant(plantName, quantity) {
+    let plant = this.plants.find((p) => p.plantName === plantName);
+    if (plant === undefined) {
+      throw new Error(`There is no ${plantName} in the garden.`);
+    }
+
+    if (!plant.ripe) {
+      throw new Error(`The ${plant.plantName} is already ripe.`);
+    }
+
+    if (plant.quantity < 0) {
+      throw new Error(`The quantity cannot be zero or negative.`);
+    }
+
+    plant.ripe = true;
+    plant.quantity += quantity;
+
+    if (quantity == 1) {
+      return `${quantity} ${plant.plantName} has successfully ripened.`;
+    } else if (quantity > 1) {
+      return `${quantity} ${plant.plantName}s have successfully ripened.`;
+    }
+  }
+
+  harvestPlant(plantName) {
+    let plant = this.plants.find((p) => p.plantName === plantName);
+    if (plant === undefined) {
+      throw new Error(`There is no ${plantName} in the garden.`);
+    }
+
+    if (plant.ripe) {
+      throw new Error(
+        `The ${plantName} cannot be harvested before it is ripe.`
+      );
+    }
+  }
+}
+
+const myGarden = new Garden(250);
+console.log(myGarden.addPlant("apple", 20));
+console.log(myGarden.addPlant("orange", 200));
+console.log(myGarden.addPlant("raspberry", 10));
+console.log(myGarden.ripenPlant("apple", 10));
+console.log(myGarden.ripenPlant("orange", 1));
+console.log(myGarden.harvestPlant("apple"));
+console.log(myGarden.harvestPlant("olive"));
+
+// The apple has been successfully planted in the garden.
+// The orange has been successfully planted in the garden.
+// The raspberry has been successfully planted in the garden.
+// 10 apples have successfully ripened.
+// 1 orange has successfully ripened.
+// The apple has been successfully harvested.
+// Uncaught Error Error: There is no olive in the garden.
