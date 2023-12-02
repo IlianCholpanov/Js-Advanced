dd303af3e8d13afa6d9177cb80da7b1d37f3d12f
Ilian Cholpanov <iliancholpanov@gmail.com>
2023-12-02 16:40:03 +0200

    Update 07-class.js

--- Exam preparation/07-class.js
+++ Exam preparation/07-class.js
@@ -11,7 +11,7 @@ class Garden {
     }
 
     this.plants.push({ plantName, spaceRequired, ripe: false, quantity: 0 });
-    this.spaceAvailable -= spaceRequired;
+    this.spaceAvailable -= Number(spaceRequired);
 
     return `The ${plantName} has been successfully planted in the garden.`;
   }
@@ -26,12 +26,12 @@ class Garden {
       throw new Error(`The ${plant.plantName} is already ripe.`);
     }
 
-    if (plant.quantity < 0) {
+    if (plant.quantity <= 0) {
       throw new Error(`The quantity cannot be zero or negative.`);
     }
 
     plant.ripe = true;
-    plant.quantity += quantity;
+    plant.quantity += Number(quantity);
 
     if (quantity == 1) {
       return `${quantity} ${plant.plantName} has successfully ripened.`;
@@ -52,14 +52,33 @@ class Garden {
       );
     }
 
-    this.plants.splice(plant, plant);
+    this.plants = this.plants.filter((p) => p.plantName !== plantName);
     this.storage.push({ plantName, quantity: plant.quantity });
-    this.spaceAvailable += plant.quantity;
+    this.spaceAvailable += plant.spaceRequired;
 
     return `The ${plantName} has been successfully harvested.`;
   }
 
-  generateReport() {}
+  generateReport() {
+    let result = [];
+    result.push(`The garden has ${this.spaceAvailable} free space left.`);
+    this.plants.sort((a, b) => a.plantName.localeCompare(b.plantName));
+    let plantsArraySorted = this.plants.map((x) => x.plantName).join(", ");
+    let plantsString = `Plants in the garden: ${plantsArraySorted}`;
+    result.push(plantsString);
+
+    let storageArrString = this.storage
+      .map((x) => `${x.plantName} (${x.quantity})`)
+      .join(", ");
+
+    let storageString =
+      this.storage.length === 0
+        ? "Plants in storage: The storage is empty."
+        : `Plants in storage: ${storageArrString}`;
+
+    result.push(storageString);
+    return result.join("\n");
+  }
 }
 
 const myGarden = new Garden(250);
@@ -68,13 +87,15 @@ console.log(myGarden.addPlant("orange", 200));
 console.log(myGarden.addPlant("raspberry", 10));
 console.log(myGarden.ripenPlant("apple", 10));
 console.log(myGarden.ripenPlant("orange", 1));
-console.log(myGarden.harvestPlant("apple"));
-console.log(myGarden.harvestPlant("olive"));
+console.log(myGarden.harvestPlant("orange"));
+console.log(myGarden.generateReport());
 
 // The apple has been successfully planted in the garden.
 // The orange has been successfully planted in the garden.
 // The raspberry has been successfully planted in the garden.
 // 10 apples have successfully ripened.
 // 1 orange has successfully ripened.
-// The apple has been successfully harvested.
-// Uncaught Error Error: There is no olive in the garden.
+// The orange has been successfully harvested.
+// The garden has 220 free space left.
+// Plants in the garden: apple, raspberry
+// Plants in storage: orange (1)
